5ef9bc92e826cb60581ea75066996e4e5464db93
Cartitza <acarstian@gmail.com>
2024-11-27 14:51:58 +0200

    initial commit

---
+++ vite-projectyes/src/Message.jsx
@@ -0,0 +1,19 @@
+
+function Message() {
+    return (
+        <div>
+            <header>
+                <h1>ChefIT</h1>
+                <nav>
+                    <ul>
+                        <li>Azi</li>
+                        <li>E</li>
+                        <li>Joi</li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
+    );
+}
+
+export default Message;
